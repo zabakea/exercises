d810f8aa1d901f474016d35cdfa271ae3d01485d
Marko Zabrdac <mark287c@stud.kea.dk>
2021-02-08 13:13:01 +0100

    Done with Name-parts

--- nameparts.js
+++ nameparts.js
@@ -14,3 +14,15 @@ const last = (fullName.substring(lastSpace+1));
 console.log(`My first name is ${first}, my middle name is ${middle} and my last name is ${last}`);
 
 
+const fullName2 = ("Albus Percival Wulfric Brian Dumbledore");
+const firstSpace2 = fullName2.indexOf(" ");
+
+const lastSpace2 = fullName2.lastIndexOf(" ");
+
+const first2 = (fullName2.substring(0, firstSpace2));
+const middle2 = (fullName2.substring(firstSpace2, lastSpace2).trim());
+const last2 = (fullName2.substring(lastSpace2+1));
+
+
+
+console.log(`My first name is ${first2}, my middle name is ${middle2} and my last name is ${last2}`);
