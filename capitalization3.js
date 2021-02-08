f22413ff05b63a8fb9744eec585a18684fa6580f
Marko Zabrdac <mark287c@stud.kea.dk>
2021-02-08 16:46:52 +0100

    3capitalization done

---
+++ capitalization3.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const singleName = "Peter";
+
+const capitalT = singleName.substring(2, 3).toUpperCase();
+
+const capi = singleName.substring(0, 2).toLowerCase();
+
+const al = singleName.substring(3);
+
+console.log(`${capi}${capitalT}${al}`);
